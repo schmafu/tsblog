8e5cb688c1598f07267757409361deceb0abd661
Hannes Prirschl <hannes.prirschl@me.com>
2016-01-16 09:05:48 +0100

    changed route

--- routes/blog.ts
+++ routes/blog.ts
@@ -2,11 +2,11 @@ import * as express from "express";
 import * as db from "../db";
 var router = express.Router();
 
-router.get("/blog",(req,res) => {
+router.get("/",(req,res) => {
     db.getBlogs((blogs) => {res.json(blogs);});
 });
 
-router.get("/blog/:id",(req,res) => {
+router.get("/:id",(req,res) => {
     db.getBlog(req.params.id,(blog) => {res.json(blog);});
 });
 
